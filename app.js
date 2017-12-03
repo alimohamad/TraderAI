3f0bccd71e51cc40e21cef50475265b3b6fb243f
Ali Mohamad <alimohamad1122@gmail.com>
2017-12-02 22:07:10 -0500

    Organized (Pull this and drop your code in Rohan thanks)

--- app.js
+++ app.js
@@ -4,20 +4,9 @@ var builder = require('botbuilder');
 var azure = require('botbuilder-azure');
 var Client = require('coinbase').Client; //Coinbase dependency
 
-var client = new Client({
-  'apiKey': 'HoOPwjQ5gPxmmrCV',
-  'apiSecret': 'krqOA9EaKWsk9FErOPVBx6YavAlnpGby',
-  'version':'2017-12-02'
-}); //coinbase client creation
 
-currencyCode = 'USD'; //currencyCode for client
-var documentDbOptions = {
-    host: 'https://trader-db.documents.azure.com:443/',
-    masterKey: 'hwRHpNoNREov4xYITQnuyH1T9sMqIcKIIV9uiuIR94YZb25StqTIZkSltHLIMBJNo9yyFgu3ausa51lNmuAWRA==',
-    database: 'botdocs',
-    collection: 'botdata'
-};
 
+/*=========================================================================AZURE BOT=========================================================================================================*/
 
 var docDbClient = new azure.DocumentDbClient(documentDbOptions);
 var cosmosStorage = new azure.AzureBotStorage({ gzipData: false }, docDbClient);
@@ -25,12 +14,11 @@ var cosmosStorage = new azure.AzureBotStorage({ gzipData: false }, docDbClient);
 var LUIS_MODEL_URL = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/6da5d499-5a63-4100-a229-d1d31cc46648?subscription-key=fe1edaedf937418d891e2482759bad8e&verbose=true&timezoneOffset=0";
 
 // Setup Restify Server
-var server = restify.createServer();
+var server = restify.createServer();s
 
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url);
 });
-
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
@@ -46,32 +34,43 @@ var bot = new builder.UniversalBot(connector, function (session) {
 server.post('/api/messages', connector.listen());
 
 //Recognizer
-var currency = "";
-var currencySymbol = "";
-var id = "";
 var recognizer = new builder.LuisRecognizer(LUIS_MODEL_URL);
 bot.recognizer(recognizer);
 
+
+
+/*=========================================================================COINBASE=========================================================================================================*/
+
+var client = new Client({
+  'apiKey': 'HoOPwjQ5gPxmmrCV',
+  'apiSecret': 'krqOA9EaKWsk9FErOPVBx6YavAlnpGby',
+  'version':'2017-12-02'
+}); //coinbase client creation
+
+currencyCode = 'USD'; //currencyCode for client
+var documentDbOptions = {
+    host: 'https://trader-db.documents.azure.com:443/',
+    masterKey: 'hwRHpNoNREov4xYITQnuyH1T9sMqIcKIIV9uiuIR94YZb25StqTIZkSltHLIMBJNo9yyFgu3ausa51lNmuAWRA==',
+    database: 'botdocs',
+    collection: 'botdata'
+};
+
+//Global Vars
+var currency = ""; //Which coin is being traded
+var currencySymbol = ""; //Coin's symbol
+var quantity = ""; //How much coin is being traded
+var id = "";
+
+
 bot.dialog('getPrice', function(session, args){ //Get Price of certain stock
-	var intent = args.intent;
-	console.log(args);
-	//console.log("\n" + intent.entities);
-	currency = ((builder.EntityRecognizer.findEntity(intent.entities, 'Cryptocurrency')).entity).toLowerCase();
-	console.log(currency);
-	if(currency == "bitcoin"){
 
-		currencySymbol = "BTC";
-	}
+  //Get Entity
+    var intent = args.intent;
+  	currency = ((builder.EntityRecognizer.findEntity(intent.entities, 'Cryptocurrency')).entity).toLowerCase();
+    if(currency == "bitcoin"){currencySymbol = "BTC";	}
+  	else if(currency == "ethereum"){currencySymbol = "ETH";	}
+  	else if(currency == "litecoin"){currencySymbol = "LTC"; }
 
-	else if(currency == "ethereum"){
-
-		currencySymbol = "ETH";
-	}
-
-	else if(currency == "litecoin"){
-
-		currencySymbol = "LTC";
-	}
 	  client.getBuyPrice({'currencyPair': (currencySymbol + '-' + currencyCode)}, function(err, price) {
 	  session.send( "The price of " + currency + " is " +  JSON.stringify(price.data.amount) + " dollars.");
 	});
@@ -79,13 +78,27 @@ bot.dialog('getPrice', function(session, args){ //Get Price of certain stock
 
 
 bot.dialog('buyCrypto', function(session){//Buy crypto
-	client.getPaymentMethods(function(err, paymentMethods) {
-  		console.log(paymentMethods);
-});
-	session.send("We aer here");
+  //Get Entities
+  var intent = args.intent;
+  currency = ((builder.EntityRecognizer.findEntity(intent.entities, 'Cryptocurrency')).entity).toLowerCase();
+  quantity = ((builder.EntityRecognizer.findEntity(intent.entities, 'quantity')).entity);
+  if(currency == "bitcoin"){currencySymbol = "BTC";	}
+  else if(currency == "ethereum"){currencySymbol = "ETH";	}
+  else if(currency == "litecoin"){currencySymbol = "LTC"; }
+
 }).triggerAction({ matches: 'buyCrypto'});
 
 
-bot.dialog('sellCrypto', function(session){//Sell crypto
+bot.dialog('sellCrypto', function(session, args){//Sell crypto
+//Get Entities
+  var intent = args.intent;
+	currency = ((builder.EntityRecognizer.findEntity(intent.entities, 'Cryptocurrency')).entity).toLowerCase();
+  quantity = ((builder.EntityRecognizer.findEntity(intent.entities, 'quantity')).entity);
+  if(currency == "bitcoin"){currencySymbol = "BTC";	}
+	else if(currency == "ethereum"){currencySymbol = "ETH";	}
+	else if(currency == "litecoin"){currencySymbol = "LTC"; }
+
 
 }).triggerAction({ matches: 'sellCrypto'});
+
+/*========================================================================SMEDGIN===========================================================================================================*/
